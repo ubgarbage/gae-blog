a43f011c9ea08ff664e60086692db598532e4e0c
Dmitry Parshin <dparshin@dparshin-macbookpro.local>
2011-01-23 00:20:04 -0500

    added exit reference

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login
 import views
 import blog.urls
 import external_auth.urls
@@ -12,7 +12,7 @@ urlpatterns = patterns( '',
                         ( '^$', views.main ),
                         ( '^admin/', include(admin.site.urls) ),
                         ( '^accounts/login/$', login, { 'template_name' : 'login.html' } ),
-                        ( '^accounts/logout/$', logout ),
+                        ( '^logout/$', views.main_logout ),
                         ( '^accounts/external/', include(external_auth.urls) ),
                         ( r'^comments/', include('django.contrib.comments.urls') ),
                         ( '^blog/', include(blog.urls) ),
--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import logout
 
 def main(request):
     return HttpResponseRedirect( '/blog/' )
+
+def main_logout(request):
+    logout(request)
+    return HttpResponseRedirect( '/' )
