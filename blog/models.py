aee0a1f3786cf647c6ef8461c8f9911ca9205243
Dmitry Parshin <dparshin@dparshin-macbookpro.local>
2011-01-22 22:43:51 -0500

    added favicon added create_time for all posts

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,13 @@ class Post(models.Model):
     title = models.CharField( max_length=255 )
     content = models.TextField()
     preview = models.TextField(blank=True)
+    create_time = models.DateField(auto_now_add=True)
 
     def __unicode__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-create_time']
 
 @receiver(pre_save, sender=Post)
 def preview_generator(sender, instance, **kwargs):
