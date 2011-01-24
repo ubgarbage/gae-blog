dc4a7558bdadc1857f98fbd1452949b9a7709f60
ub <ub.garbage@gmail.com>
2011-01-24 18:08:13 -0500

    user name added to blog pages

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-# Create your views here.
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, redirect
 from django.core.context_processors import csrf
 from django.contrib.comments.models import Comment
+from django.template import RequestContext
 from utils import get_view_url
 from models import Post
 
@@ -15,12 +15,16 @@ def main(request):
 def posts(request):
     posts = Post.objects.all()
     comments = Comment.objects.order_by('-submit_date')[0:10]
-    return render_to_response( 'posts.html', {'posts': posts, 'comments': comments} )
+    context = RequestContext( request, {'posts': posts, 'comments': comments} )
+    return render_to_response( 'posts.html', context )
 
 @login_required
 def post(request, post_id):
     post_obj = Post.objects.get( id=post_id )
-    c = { 'post':post_obj, 'username':request.user.username, 'redirect_after_comment':get_view_url(request, post, [post_id]) }
-    c.update( csrf(request) )
-    return render_to_response( 'post.html', c )
+    context = RequestContext( request, 
+                              { 'post':post_obj, 
+                                'username':request.user.username, 
+                                'redirect_after_comment':get_view_url(request, post, [post_id]) } )
+    context.update( csrf(request) )
+    return render_to_response( 'post.html', context )
 
