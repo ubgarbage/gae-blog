f6615970584030e6b3081608939373561eb6d2fd
ub <ub.garbage@gmail.com>
2011-01-26 17:28:28 -0500

    added author to each post

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField( max_length=255 )
     content = models.TextField()
     preview = models.TextField(blank=True)
     create_time = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.title
