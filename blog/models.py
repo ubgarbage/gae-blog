3e49b780d9dcb9df8a8d2bd38d47ee21eccbc0e8
ub.garbage <ub.garbage@gmail.com>
2011-02-02 23:59:57 -0500

    started to implement email notifications

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,11 @@
+ # -*- coding: utf-8 -*-
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.core.mail import send_mail 
+from django.template import loader, Context
+import logging
 
 class Post(models.Model):
     title = models.CharField( max_length=255 )
@@ -24,8 +28,30 @@ def preview_generator(sender, instance, **kwargs):
     words = instance.content.split(' ')[:20]
     instance.preview = ' '.join(words)
 
+@receiver(pre_save, sender=Post)
+def on_new_post(sender, instance, **kwargs):
+    tmpl = loader.get_template( 'email_new_post.txt' )
+    #TODO: check if post existed
+    send_email_to_subscribers( unicode("У нас новая статья!", "utf-8"), tmpl.render( Context({'title':instance.title}) ) )
+
 class Subscriber(models.Model):
     user = models.ForeignKey(User, unique=True)
 
     def __unicode__(self):
         return self.user.username
+
+def get_subscriber( subuser ):
+    try:
+        return Subscriber.objects.get(user=subuser)
+    except Subscriber.DoesNotExist:
+        return None
+
+def send_email_to_subscribers( subject, content ):
+    for subscriber in User.objects.all():
+        if None != get_subscriber( subscriber ):
+            try:
+                logging.info( 'Sending "' + subject + '" to ' + subscriber.username + ' on ' + subscriber.email )
+                send_mail( subject, content, 'author@fromrussiatous.com', [subscriber.email] )
+            except Exception, e:
+                logging.warning( 'Failed to send email to ' + subscriber.username + ' on ' + subscriber.email + ': ' + str(e) )
+        
