6bd4e8384e034fcbe0271e5e84a7fea99e2ab7ab
Dmitry Parshin <parshin@corp.mail.ru>
2011-02-12 18:10:51 -0500

    fixed mail list

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, send_mail
 from django.template import loader, Context
 import logging
 
@@ -34,7 +34,7 @@ def on_new_post(sender, instance, **kwargs):
     if not exist:
         body = loader.get_template( 'email_new_post.txt' )
         subj = loader.get_template( 'email_new_post_subj.txt' )
-        send_email_to_subscribers( u"У нас новая статья!", body.render( Context({'title':instance.title}) ) )
+        send_email_to_subscribers( subj.render( Context({'title':instance.title}) ), body.render( Context({'title':instance.title}) ) )
     else:
         logging.info( 'post changed, we are not going to send notification' )
 
@@ -53,15 +53,9 @@ def get_subscriber( subuser ):
 def send_email_to_subscribers( subject, content ):
     for subscriber in User.objects.all():
         if None != get_subscriber( subscriber ):
-            # try:
+            try:
                 logging.info( 'Sending "' + subject + '" to ' + subscriber.username + ' on ' + subscriber.email )
-                send_email( subject, content, 'author@fromrussiatous.com', subscriber.email )
-            # except Exception, e:
-            #     logging.warning( 'Failed to send email to ' + subscriber.username + ' on ' + subscriber.email + ': ' + str(e) )
+                send_mail( subject, content, 'parshin.da@gmail.com', [subscriber.email] )
+            except Exception, e:
+                logging.warning( 'Failed to send email to ' + subscriber.username + ' on ' + subscriber.email + ': ' + str(e) )
         
-def send_email( subject, content, from_mail, to_mail ):
-#    send_mail( subject, content, 'author@fromrussiatous.com', [subscriber.email] )
-    msg = EmailMessage( subject, content, from_mail, [to_mail], 
-                        headers = {'Reply-To': 'parshin.da@gmail.com'})
-    import sys
-    sys.stderr.write( msg.message() )
